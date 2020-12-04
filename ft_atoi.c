843215b921e7276ca7b6cbab8fc26b25d78435f4
LoloLaD <loicfourmaux@gmail.com>
2020-12-04 14:01:40 +0100

    ouliloulex

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfourmau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lfourmau <lfourmau@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:16:57 by lfourmau          #+#    #+#             */
-/*   Updated: 2020/11/26 01:34:09 by lfourmau         ###   ########lyon.fr   */
+/*   Updated: 2020/12/04 13:57:17 by lfourmau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ static int	ft_isspace(char c)
 
 int			ft_atoi(const char *str)
 {
-	int i;
-	int j;
-	int res;
-	int sign;
+	int				i;
+	int				j;
+	unsigned long	res;
+	int				sign;
 
 	sign = 1;
 	res = 0;
@@ -41,9 +41,9 @@ int			ft_atoi(const char *str)
 		res = res * 10 + str[i++] - 48;
 		j++;
 	}
-	if (j > 18 && sign == -1)
+	if (res > 9223372036854775807 && sign == -1)
 		return (0);
-	else if (j > 18 && sign == 1)
+	else if (res > 9223372036854775807 && sign == 1)
 		return (-1);
 	return (res * sign);
 }
